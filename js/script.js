42299e42068cf25281c21e016602800047572a8e
Vieyra <vieyraurenabravo@gmail.com>
2020-09-15 20:23:13 -0600

    Agrego cambios en la funcionalidad de las tabs

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 const tabs = document.querySelectorAll('.tabs-tablist-item');
 const content = document.querySelectorAll('.tabs-content-item');
 
-
-
-function buttonClick(event){
-  event.preventDefault();
-  const link = event.currentTarget.getAttribute('href');
-  const id = link.substring(1);
-  console.log(id);
-  
-  for (let index = 0; index < content.length; index++){
-    if (content[index].getAttribute('id') == id){
-      content[index].style.display = 'block';
-    }
-  }  
-}
-
-function hover() {
-  for (let index = 0; index < tabs.length; index++){
-    tabs[index].style.borderBottom = 'none';  
-    tabs[index].style.backgroundColor = '#fff';      
-  }
-}
-
-
 for (let index = 0; index < content.length; index++){
-  content[index].style.display = 'none';
+  //content[index].style.display = 'none';
+  content[index].classList.add('js-content-hidden');
 }
-content[0].style.display = 'block';
+//content[0].style.display = 'block';
+content[0].classList.remove('js-content-hidden');
 
+tabs[0].classList.add('js-active');
 
 for (let index = 0; index < tabs.length; index++){
-  tabs[index].addEventListener('click', buttonClick); 
-  tabs[index].addEventListener('click', hover); 
+  tabs[index].addEventListener('click', (event) => {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const href = element.getAttribute('href');
+    const id = href.substring(1);
+    console.log(id);
+    for (let index = 0; index < content.length; index++) {
+      //content[index].style.display = 'none';
+      content[index].classList.add('js-content-hidden');
+      if (content[index].getAttribute('id') == id){
+        //content[index].style.display = 'block';
+        content[index].classList.remove('js-content-hidden');
+      }
+    }
+
+    for (let index = 0; index < tabs.length; index++) {
+      //tabs[index].style.backgroundColor = '#eee';
+      tabs[index].classList.remove('js-active');
+    }
+    //event.currentTarget.style.backgroundColor = '#fff'
+    event.currentTarget.classList.add('js-active');
+  })
 }
